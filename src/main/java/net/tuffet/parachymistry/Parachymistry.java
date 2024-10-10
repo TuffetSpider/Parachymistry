ebdcaa9b47d76c95f51aa2d8a0ab877e63100eb6
TuffetSpider <tuffetspider616@gmail.com>
2024-10-10 17:47:33 -0400

    test

---
+++ src/main/java/net/tuffet/parachymistry/Parachymistry.java
@@ -0,0 +1,24 @@
+package net.tuffet.parachymistry;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Parachymistry implements ModInitializer {
+	public static final String MOD_ID = "parachymistry";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
